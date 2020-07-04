f798b70941879710170ae12ef1a041fb3d9a4c11
SergeiTSV <57957999+SergeiTSV@users.noreply.github.com>
2020-07-05 00:27:33 +0300

    Add files via upload
    Simple

---
+++ Sum.java
@@ -0,0 +1,15 @@
+package by.htp.les01.logic;
+
+public class Sum {
+
+	public static void sum(int x, int y) {
+
+		int z;
+
+		z = x + y;
+		
+		System.out.println("z =" + z);
+
+	}
+
+}
---
+++ Test.java
@@ -0,0 +1,19 @@
+package by.htp.les01.logic;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0;
+		int y = 0;
+		while (x < 5) {
+			y =  x - y;
+
+			System.out.print(x + "" + y + " ");
+		
+		x = x + 1;
+		}
+
+	}
+
+}
